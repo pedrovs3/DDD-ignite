8a10f09cc9b16c8d809615dc80c8a8cefea1743c
Pedro Vieira <pedro.silva@pagtel.com.br>
2023-12-05 15:10:20 -0300

    feat: add test to paginated question answers

--- src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -1,23 +1,18 @@
-import {InMemoryQuestionsRepository} from 'tests/repositories/in-memory-questions-repository';
-import {makeQuestion} from "../../../../../tests/factories/make-question.factory";
 import {InMemoryAnswerRepository} from "../../../../../tests/repositories/in-memory-answer-repository";
 import {makeAnswer} from "../../../../../tests/factories/make-answer.factory";
 import {UniqueEntityId} from "@/core/entities";
 import {FetchQuestionAnswersUseCase} from "@domain/forum/application/use-cases/fetch-question-answers";
 
-let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let inMemoryAnswerRepository: InMemoryAnswerRepository;
 let sut: FetchQuestionAnswersUseCase;
 
 describe('Fetch question answers', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
     inMemoryAnswerRepository = new InMemoryAnswerRepository();
     sut = new FetchQuestionAnswersUseCase(inMemoryAnswerRepository);
   });
 
   it('should be able to fetch question answers', async () => {
-    await inMemoryQuestionsRepository.create(makeQuestion({createdAt: new Date('2021-01-01')}, new UniqueEntityId('1')));
     await inMemoryAnswerRepository.create(makeAnswer({
       createdAt: new Date('2021-01-01'),
       questionId: new UniqueEntityId('1')
@@ -39,5 +34,16 @@ describe('Fetch question answers', () => {
       expect.objectContaining({questionId: new UniqueEntityId('1'), createdAt: new Date('2021-01-02')}),
       expect.objectContaining({questionId: new UniqueEntityId('1'), createdAt: new Date('2021-01-01')}),
     ]);
+  });
+
+  it('should be able to fetch paginated question answers', async () => {
+    for (let i = 0; i < 15; i++) {
+      await inMemoryAnswerRepository.create(makeAnswer({questionId: new UniqueEntityId('1')}));
+    }
+
+    const {answers} = await sut.execute({page: 2, limit: 10, questionId: '1'});
+
+    expect(answers.length).toBe(5);
+    expect(answers[0].createdAt.getUTCDate()).toBeGreaterThanOrEqual(answers[1].createdAt.getUTCDate());
   });
 });
